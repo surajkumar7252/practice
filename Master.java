bb0efc7afd5fbc16e14713b3ba84325bbe145ec4
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 18:18:05 +0530

    uc1 commit

--- Master.java
+++ Master.java
@@ -6,7 +6,13 @@ public class Master{
 	  public static void main(String[] args) 
 	              {
 		  System.out.println("Welcome to Employee Wage Computation Program");    // prints the message.
-
+		  int IS_FULLTIME=1;                                     // CONSTANT is defined.
+			System.out.println("Employee Attendance check");       
+			int a= (int)Math.floor(Math.random()*10)%2;            // computes the value of a.
+			if(a==IS_FULLTIME)                                      // checks if the value of a is equal to Employee.
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is absent");
 	               }
 
 	            }
