455f1c5a36c91b9c53a7b3331911aa587529c9ed
Surajmal Kumar <surajkumar7252@gmail.com>
2020-09-29 18:14:30 +0530

    master commit

---
+++ Master.java
@@ -0,0 +1,15 @@
+/*
+	A simple master program to print a message "Welcome to Emploee Wage Computation Program".
+	*/
+public class Master{
+	     
+	  public static void main(String[] args) 
+	              {
+		  System.out.println("Welcome to Employee Wage Computation Program");    // prints the message.
+
+	               }
+
+	            }
+
+
+
